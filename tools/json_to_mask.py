739834ec6703166d63b0ca01669dd95847802371
Your Name <kewa990809@gmail.com>
2024-11-26 22:26:35 -0500

    update convert function

--- tools/json_to_mask.py
+++ tools/json_to_mask.py
@@ -1,15 +1,20 @@
+import argparse
+
 import os
 import json
 
+# input is a folder with images folder and some json-mini,
+# output one processed json for all images
+# brush_info is a list of brush
 def concate_json(folder_path):
 
   out_json = []
 
   # read all json file
-  for file in os.listdir():
+  for file in os.listdir(folder_path):
     if file.endswith('.json'):
       # Read JSON file
-      with open(file, 'r') as file:
+      with open(folder_path+file, 'r') as file:
         data = json.load(file)
 
         # read each annotation in json
@@ -20,7 +25,7 @@ def concate_json(folder_path):
 
           annotation = {}
           if 'brush' in anno:
-            annotation['brush_info'] = anno['brush'][0]
+            annotation['brush_info'] = anno['brush']
           if 'bbox' in anno:
             annotation['bbox'] = anno['bbox']
           # TODO: polygon info and crop_box
@@ -37,12 +42,13 @@ def concate_json(folder_path):
   with open(json_file_path+"data.json", 'w') as json_file:
       json.dump(out_json, json_file, indent=4)
 
+
 # copied from label studio github
 # https://stackoverflow.com/questions/74339154/how-to-convert-rle-format-of-label-studio-to-black-and-white-image-masks
 
 from typing import List
 import numpy as np
-
+import cv2
 
 class InputStream:
     def __init__(self, data):
@@ -103,7 +109,9 @@ def rle_to_mask(rle: List[int], height: int, width: int) -> np.array:
     image = np.reshape(out, [height, width, 4])[:, :, 3]
     return image
 
-def save_mask(json_file_path, mask_path)
+# input is the folder with images and json
+# output to a mask folder with all masks where each obj in instance id
+def save_mask(json_file_path, mask_path):
     # save the masks to data/masks/
     with open(json_file_path, 'r') as file:
       data = json.load(file)
@@ -114,28 +122,66 @@ def save_mask(json_file_path, mask_path)
 
         if not anno['annotation'] or 'brush_info' not in anno['annotation']: continue
 
-        brush = anno['annotation']['brush_info']
-        rle = brush['rle']
-        height = brush['original_height']
-        width = brush['original_width']
-        mask = rle_to_mask(rle, height, width)
-        print(mask.shape)
-        print(mask.dtype)
+        empty_mask = np.zeros((224,224))
+        cnt = 0
+        for i,brush in enumerate(anno['annotation']['brush_info']):
+          rle = brush['rle']
+          mask = rle_to_mask(rle, 224, 224)
 
+          # Convet the mask into binary
+          binary_mask = (mask > 0).astype(np.uint8)
+
+          # Find the connected components in the image
+          num_labels, labels_im = cv2.connectedComponents(binary_mask)
+
+          # print(mask.shape)
+          # print(mask.dtype)
+          # print(np.unique(mask))
+          for label in range(1, num_labels):  # Start from 1 to skip the background
+            individual_mask = (labels_im == label).astype(np.uint8)
+            if individual_mask.sum() < 5: continue
+            empty_mask[individual_mask>0] = cnt+1
+            cnt += 1
+
+        # Check if the directory exists
+        if not os.path.exists(mask_path):
+            # Create the directory
+            os.makedirs(mask_path)
         # Define the path where you want to save the array
         file_path = mask_path+'{}.npy'.format(anno['image'].split('.')[0])
 
         # Save the array to a .npy file
-        np.save(file_path, mask > 0)
+        np.save(file_path, empty_mask)
 
+def show_mask(mask, ax, random_color=False, borders = True):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.1])
+    h, w = mask.shape[-2:]
+    mask = mask.astype(np.uint8)
+    mask_image =  mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    if borders:
+        import cv2
+        contours, _ = cv2.findContours(mask,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        # Try to smooth contours
+        contours = [cv2.approxPolyDP(contour, epsilon=0.01, closed=True) for contour in contours]
+        mask_image = cv2.drawContours(mask_image, contours, -1, (1, 1, 1, 0.5), thickness=1)
+    ax.imshow(mask_image)
 
-import argparse
 
 def main(args):
-    output, output_name = refine_images(args.image_dir, args.mask_dir, args.batch_sz)
+    folder_path = "/content/mnMask-2/"
+    concate_json(folder_path)
 
-    for i in range(len(output)):
-        np.save(os.path.join(args.output_path,output_name[i]), output[i])
+    mask_path = "/content/mnMask-2/masks/"
+    json_file_path = "/content/mnMask-2/data.json"
+    save_mask(json_file_path, mask_path)
+
+    # masks = np.load("/content/mnMask-2/masks/GFP-H2B_A4_1_2023y07m01d_11h02m-1.npy")
+    # im = Image.open("/content/mnMask-2/images/GFP-H2B_A4_1_2023y07m01d_11h02m-1.png")
+    # plt.imshow(im)
+    # show_mask(masks>0,plt)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="process vague mask input to better segment mask")
