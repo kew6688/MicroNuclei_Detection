46ffad2e5632406eadcca783786fd4c5e0a99019
Yukai Wang <y3229wan@cedar1.int.cedar.computecanada.ca>
2024-12-16 11:20:50 -0800

    update bash scripts

--- compute_scripts/image_process_manual.sh
+++ compute_scripts/image_process_manual.sh
@@ -2,51 +2,12 @@
 #SBATCH --nodes=1
 #SBATCH --gpus-per-task=1
 #SBATCH --mem=8G 
-#SBATCH --time=00:50:00
+#SBATCH --time=00:30:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=markus.wang@mail.utoronto.ca
 #SBATCH --job-name=test
 #SBATCH --output=test.o
 #SBATCH --error=test.e
-
-# ---------------------------------------------------------------------------------------------
-echo "Create a virtual env to run"
-# ---------------------------------------------------------------------------------------------
-
-module load python/3.11
-virtualenv --no-download $SLURM_TMPDIR/env
-source $SLURM_TMPDIR/env/bin/activate
-pip install --no-index --upgrade pip
-
-# pip install --no-index -r requirements.txt
-
-# ---------------------------------------------------------------------------------------------
-echo "Install SAM2"
-# ---------------------------------------------------------------------------------------------
-
-# install SAM2
-cd $SLURM_TMPDIR
-git clone https://github.com/facebookresearch/sam2.git
-cd sam2
-pip install -e .
-
-# download checkpoints
-cd checkpoints && \
-./download_ckpts.sh && \
-cd ..
-
-# ---------------------------------------------------------------------------------------------
-echo "Install mn detection packge"
-# ---------------------------------------------------------------------------------------------
-
-# Install package
-cd $SLURM_TMPDIR
-git clone https://github.com/kew6688/MicroNuclei_Detection.git 
-cd MicroNuclei_Detection
-pip install --no-index -e .
-
-# Download checkpoints
-git clone https://huggingface.co/kew1046/MaskRCNN-resnet50FPN
 
 # ---------------------------------------------------------------------------------------------
 echo "Prepare data"
@@ -69,18 +30,16 @@ echo "Start main process"
 # Example:
 #       >>> python image_process.sh /home/scratch/test test.json ALL
 
-cd $SLURM_TMPDIR
-python MicroNuclei_Detection/compute_scripts/image_process.py \
+python image_process.py \
     --src $1 \
     --dst $2 \
     --mode $3
 
+echo $1
        
 # ---------------------------------------------------------------------------------------------
 echo "Save output"
 # ---------------------------------------------------------------------------------------------
-
-cp $2 ~/scratch
 
 # The computations are done, so clean up the data set...
 # cd $SLURM_TMPDIR
