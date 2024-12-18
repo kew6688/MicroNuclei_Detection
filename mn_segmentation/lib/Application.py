d691dd68278c39b2b9483923e6ff692803f5b2f4
Yukai Wang <58201273+kew6688@users.noreply.github.com>
2024-12-18 18:25:47 -0500

    Update Application.py

--- mn_segmentation/lib/Application.py
+++ mn_segmentation/lib/Application.py
@@ -19,7 +19,7 @@ def get_device():
   if torch.cuda.is_available():
     return torch.device('cuda')
   elif torch.backends.mps.is_available():
-    device = torch.device("mps")
+    return torch.device("mps")
   else:
     return torch.device('cpu')
 
